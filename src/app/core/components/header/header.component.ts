8f7716a644e96d6487316cb9c5d8b667a98c728c
robertIsaac <dev.robert.isaac@gmail.com>
2023-05-06 20:31:29 +0300

    add fr and ar translation and add dropdown in header to switch between them

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterLink, CollapseModule, RouterLinkActive, TranslateModule],
+  imports: [CommonModule, RouterLink, CollapseModule, RouterLinkActive, BsDropdownModule, TranslateModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  private readonly translateService = inject(TranslateService);
+  protected readonly locales = ['en', 'fr', 'ar'];
   protected isCollapsed = true;
+
+  protected changeLanguage(locale: string): void {
+    this.translateService.use(locale);
+  }
 }
